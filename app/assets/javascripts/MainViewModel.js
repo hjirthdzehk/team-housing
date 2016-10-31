0adcb648c631be0bbb7029d31d6871e98100508b
Oleg Kulaev <vilor9473@gmail.com>
2016-10-31 16:30:33 +0300

    add sammy.js for routing, extract template to partial-view

---
+++ app/assets/javascripts/MainViewModel.js
@@ -0,0 +1,29 @@
+var MainViewModel = function() {
+    var self = this;
+
+    this.template = ko.observable({});
+    var swapTemplate = function(template) {
+        if (self.template().model && _.isFunction(self.template().model.dispose)) {
+            self.template().model.dispose();
+        }
+
+        self.template(template);
+    };
+
+    var app = Sammy('#main', function() {
+        this.get('#/measuresInput', function() {
+            $.get('/meters/byFlatId/1').then(function(groups) {
+                var viewModel = new MeasuresInputViewModel(groups)
+                swapTemplate({
+                    name: 'measures-input-template',
+                    model: viewModel
+                });
+            });
+        });
+    });
+
+    this.run = function() {
+        app.run('#/measuresInput');
+    }
+    ko.applyBindings(self, $('#main')[0]);
+}
