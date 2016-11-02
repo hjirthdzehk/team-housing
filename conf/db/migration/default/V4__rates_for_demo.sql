c74eee84b163d0ac8f0495be13b1d39908c5114f
Anton Kiselev <straw.berry.pie@ya.ru>
2016-11-02 10:10:21 +0300

    Rates for demo.

---
+++ conf/db/migration/default/V4__rates_for_demo.sql
@@ -0,0 +1,5 @@
+insert into rate (rate_id, value, date_from, date_to, meter_unit_id)
+values (1, 228, '2016-10-02', '2016-11-30', 1);
+
+insert into rate (rate_id, value, date_from, date_to, meter_unit_id)
+values (2, 42, '2016-10-02', '2016-11-30', 2);
