2b900813096b82fdef675fa0687ee5a6428d667c
Vlad <vladt9@yandex.ru>
2016-11-17 15:59:06 +0300

    Added Service Request controller

---
+++ app/controllers/ServiceRequests.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import models.ServiceRequest
+import javax.inject.Inject
+
+import com.github.tototoshi.play2.json4s.Json4s
+import org.joda.time.DateTime
+import org.json4s.{DefaultFormats, Extraction}
+import org.json4s.ext.JodaTimeSerializers
+import play.api.mvc.{Action, Controller}
+
+/**
+  * Created by VladVin on 17.11.2016.
+  */
+class ServiceRequests @Inject()(json4s: Json4s) extends Controller {
+    import json4s._
+
+    implicit val formats = DefaultFormats ++ JodaTimeSerializers.all
+
+//    def all(flatId: Long) = Action {
+//        var requests = ServiceRequest.findAllForFlat(flatId)
+//        var nextVisits = Seq[DateTime]
+//        requests.foreach(sr => nextVisits + ServiceRequest.findNextVisitDate(sr.id))
+//        Ok
+//    }
+}
