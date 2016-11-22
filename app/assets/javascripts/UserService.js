0767e4130c25092f5da20908e5bea273bd10d364
Oleg Kulaev <vilor9473@gmail.com>
2016-11-22 03:54:34 +0300

    fix login and cookie problems

--- app/assets/javascripts/UserService.js
+++ app/assets/javascripts/UserService.js
@@ -1,16 +1,43 @@
 /** Created by a.kiselev on 22/11/2016. */
 
 var UserService = function () {
+    var setCookie = function (cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    };
+
+    var getCookie = function (cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    };
+
     this.login = function (login, pass) {
         return $.post('/dwellers/signIn', {
             'email': login,
             'passwordHash': md5(pass)
-        }).done(function (personId) {
-            Cookies.set('person_id', personId, {path: "/", expires: 7});
+        }).then(function(personId) {
+            setCookie("personId", personId);
+            return $.Deferred().resolve(personId).promise();
         });
     };
 
     this.getPersonId = function () {
-        return Cookies.get('person_id');
-    }
+        return getCookie("personId");
+    };
+
+    this.isLoginedAsUser = function() {
+        return !!getCookie("personId");
+    };
 };
